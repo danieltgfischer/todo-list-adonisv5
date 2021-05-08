bb00762e3d2e210439d31bed989fc541d70c9bc0
Daniel Tiago Fischer <danieltgfischer.@gmail.com>
2021-05-08 12:15:13 -0300

    fix: config folder permissions

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -2,10 +2,22 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
 
 export default class UsersController {
+  public async index({}: HttpContextContract) {
+    return await User.all()
+  }
   public async store({ request, response }: HttpContextContract) {
     try {
+      const userExists = await User.findBy('email',  request.input('email') )
+      if (userExists) {
+        return response.send({
+          error: {
+            message: 'Usuário já existente'
+          }
+        })
+      }
       const data = request.all()
-      await User.create(data)
+      const user = await User.create(data)
+      return user
     } catch (error) {
       return response.status(500).send({
         error: {
